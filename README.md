53168c8f0df9cb08880fc4638b18ac449a4091a0
jouriena11 <117335220+jouriena11@users.noreply.github.com>
2022-12-14 23:44:26 +1100

    upload HTML, JS, CSS

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# W5-challenge-scheduler-calendar-app
+# **W5-challenge-scheduler-calendar-app**
+
+In week 5 challenge, we're assigned to fix HTML and JavaScript codes to make the scheduler-calendar web application functional. The key requirements are:
+
+- To add current date to the header
+
+- To add more hourly rows. To resemble the mockup, I also make the scheduler only available between 9am to 5pm.
+
+- To style the rows so that past item(s) has gray background, present item has red background, and future item(s) has green background. These styles (provided as a starter code) already exist in the CSS file using the same class name (i.e., .past .present .future)
+
+- To save a scheduled item/content to a browser localStorage when the corresponding save button is clicked.
+
+- To retrieve and display the saved scheduled item/content on the web application when the page is refreshed.
+
+## Approaches to Solving the Challenge
+
+**Functions Grouping**
+Init -- get data + display 
+Get Data
+Render Data
+Save Data
+
+**Rows of textarea / scheduled items input**
+Although starter codes are provided in the HTML, I understand that they only serve simply as examples of rendered results (i.e., past = gray, preset = red, and future = green), and that we don't have to keep them in the HTML files. After all, hardcoding the rest of the hourly rows in the HTML file would result in unnecessary lengthy codes, especially when this can be automatically generated with JavaScript with loop. In reference to Week 5 Student Activity # 10, it's also possible to assign a variable with the whole <div> element as value. Hence, this is deemed a much more efficient way to create scheduled item elements. These codes are put together inside the timeBlockRender() function.
+
+**Deciding when to code with JavaScript and jQuery**
+I've been told that JavaScript codes are processed at a much faster speed comparing to jQuery codes, regardless of actual human perception (i.e., the difference in speed might not be clearly noticeable to human users in real life), whereas jQuery make it much easier to write codes. But a good web developer should know how to code in both JavaScript and jQuery (as well as any other programming languages that are currently in demand)
+
+
+const -- to avoid using the same variable name
+let -- to make sure that its use is limited within a scope
+
+## **Extended Personal Study**
+- insertAdjacentHTML (https://css-tricks.com/comparing-methods-for-appending-and-inserting-with-javascript/)
+
+
+## **Lessons Learnt**
+- day.js().format('format') would return a string. To convert the string into a number so that calculation or comparison can be performed, it must be wrapped with number() method.
+
+- 
---
+++ assets/script.js
@@ -0,0 +1,134 @@
+// Display the current date in the header of the page.
+const today = dayjs();
+$('#currentDay').text(today.format('dddd, MMMM D, YYYY'));
+
+const currentHr = dayjs().format('H'); // this is a string ; H = 24-hour format
+const currentHrNum = Number(currentHr); // convert currentHr into a number
+
+/* Time Display -- not required but it helps inspecting whether or not the time block is rendered correctly or not. 
+Besides, in real life, this is deemed convenient to a user, as there's no need to check the time elsewhere. */
+
+const timeDisplayEl = $('#time-display');
+
+function displayTime() {
+  var rightNow = dayjs().format('hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
+displayTime();
+setInterval(displayTime, 1000);
+
+/* Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+the code isn't run until the browser has finished rendering all the elements
+in the html.*/
+
+$(document).ready(function() {
+  console.log("Page is loaded and ready to run codes.")
+  init();
+});
+
+// Codes to execute when a save button is clicked
+
+var scheduleItems = [];
+
+function saveItem(event) { // event = button click
+  const buttonEl = event.target;
+  const parentID = buttonEl.parentElement.id;
+  const textAreaEl = buttonEl.parentElement.children[1]
+
+  // Saving an empty textarea is not allowed. If the input textarea is blank when saved, the following alert message would appear.
+  if(textAreaEl.value.trim() === '') {
+    alert('Saving a blank field is not allowed. Please enter some text.');
+    return;
+  }
+
+  // Checking if the scheduleItems array contains any saved data by using findIndex() array method.
+  const foundIndex = scheduleItems.findIndex((item) => { 
+    return item.time === parentID.split('-')[1];
+  })
+
+  if(foundIndex === -1) { // If no data is found (i.e., findIndex() returns a value of -1), then add the new data to the array.
+    scheduleItems.push({
+      value: textAreaEl.value.trim(), // use .trim() to remove any blank space before and after the input content
+      time: parentID.split('-')[1], // use split() to split hour-i into "hour" and "i", then select i which has an index of 1
+    })
+  }
+  else {
+    scheduleItems[foundIndex] = { // If saved data is found (i.e., findIndex() returns a value other than -1), then replace the data in that particular index with new data.
+      value: textAreaEl.value.trim(),
+      time: parentID.split('-')[1],
+    }
+  }
+  localStorage.setItem('storeItem', JSON.stringify(scheduleItems)); // Save the latest array data to localStorage
+}
+
+// Render time block container
+
+function timeBlockRender() {  
+  
+  const timeBlockEl = document.getElementById('time-block-container');
+
+  for(var i = 9; i <= 17; i++) { // Time block is only available from 9am to 5pm to resemble the mock-up.
+    
+    let hourStr = i + 'AM';
+    if(i == 12) {
+      hourStr = i + 'PM';
+    }
+    else if(i > 12) {
+      hourStr = (i-12)+'PM'; // e.g. if i = 13, then 1PM is returned.
+    }
+
+    // Change container CSS class (past/present/future) by comparing i to current time
+    let bgClass = 'past';
+    if (i === currentHrNum) {
+      bgClass = 'present';
+    } else if (i < currentHrNum) {
+      bgClass = 'past';
+    } else {
+      bgClass = 'future';
+    }
+
+    /* When refreshing the page, the saved item should still be displayed on the web app. 
+    This is again achieved by using findIndex() method and conditional statements. */
+    
+    const foundIndex = scheduleItems.findIndex((item) => {
+      return item.time == i
+    })
+
+    let textAreaStr = "";
+    if(foundIndex === -1) { // if no saved data is found (i.e., findIndex() returns -1), then returns empty string.
+      textAreaStr = "";
+    }
+    else {
+      textAreaStr = scheduleItems[foundIndex].value; // if saved data is found, then returns the saved data
+    }
+
+    // Reference to Week 5 student activity #10, but this is written in JavaScript instead of jQuery
+    let html = `<div id="hour-${i}" class="row time-block ${bgClass}">
+                  <div class="col-2 col-md-1 hour text-center py-3">${hourStr}</div>
+                  <textarea class="col-8 col-md-10 description" rows="3">${textAreaStr}</textarea>
+                  <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+                    <i class="fas fa-save pe-none" aria-hidden="true"></i>
+                  </button>
+                </div>`;
+    timeBlockEl.insertAdjacentHTML('beforeend',html); // reference: https://css-tricks.com/comparing-methods-for-appending-and-inserting-with-javascript/.
+  }
+}
+
+// Apply addEventListener to saveBtn with jQuery
+
+function addEventListenerSaveButton() {
+  $('.saveBtn').on('click', function(event) {
+    saveItem(event);
+  });
+}
+
+function init() {
+  scheduleItems = [];
+  var schedulerCalendar = JSON.parse(localStorage.getItem('storeItem'));
+  if (schedulerCalendar !== null) {
+      scheduleItems = schedulerCalendar;
+  }
+
+  timeBlockRender();
+  addEventListenerSaveButton();
+}; 
